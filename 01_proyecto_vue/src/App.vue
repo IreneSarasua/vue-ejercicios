e6158142cdbd91fca0957a2c2e2fa7af95b10d70
Irene Sarasua <irene.sarasua@ikasle.egibide.org>
2024-04-16 11:20:06 +0200

    directivas 1

---
+++ 01_proyecto_vue/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+const nombre = 'Egibide';
+</script>
+
+<template>
+    <div class="container pt-5">
+        <h1>¡Hola {{ nombre }}!</h1>
+        <p class="alert alert-primary">Esto es una prueba.</p>
+        <p class="alert alert-secondary">¿Apereceré?</p>
+    </div>
+</template>
+
+<style scoped>
+h1 {
+    color: lightseagreen;
+
+}
+</style>
---
+++ 02_directivas/ej1/src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+    import { ref } from 'vue';
+    const datos = ref({
+        nombre: '',
+        apellido: ''
+    })
+
+
+</script>
+
+
+<template>
+
+    <div class="container w-50">
+        <h2>Datos Personales</h2>
+        <div class=" border border-1 border-black rounded p-3">
+            <div class="mb-3">
+                <label class="form-label" for="nombre">Nombre</label>
+                <input class="form-control" id="nombre" type="text" v-model="datos.nombre"/>
+            </div>
+            <div class="mb-3">
+                <label class="form-label" for="nombre">Apellido</label>
+                <input class="form-control" id="nombre" type="text" v-model="datos.apellido"/>
+            </div>
+            <div class="alert alert-warning">
+                Tu nombre completo es: {{ datos.nombre }} {{ datos.apellido }}
+            </div>
+
+        </div>
+
+    </div>
+
+
+</template>
+
+<style scoped>
+
+</style>
