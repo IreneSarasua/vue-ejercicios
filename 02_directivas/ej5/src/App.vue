c931aa82e8b14cad3e6179751384ab7fb2593000
Irene Sarasua <irene.sarasua@ikasle.egibide.org>
2024-04-17 10:58:30 +0200

    directivas 5

--- 02_directivas/ej5/src/App.vue
+++ 02_directivas/ej5/src/App.vue
@@ -1,16 +1,30 @@
 <script setup>
-const nombre = 'Nieves';
+import {ref} from 'vue'
+
+//const miArray = ref([]);
+const miArray = ref(['Elemento 1', 'Elemento 2', 'Elemento 3']);
+
 </script>
 
 <template>
-    <div class="container pt-5">
-        <h1>¡Hola {{ nombre }}!</h1>
-        <p class="alert alert-success">Esto es una prueba.</p>
+    <div class="container w-50 pt-3">
+        <h2>Lista de elementos</h2>
+
+        <ul class="list-group w-100">
+            <li class="list-group-item d-flex justify-content-between align-items-center"
+                v-for="elem of miArray">
+                {{ elem }}
+            </li>
+        </ul>
+
+        <div class="alert alert-danger" v-show="miArray.length === 0">
+            No hay elementos en la lista.
+        </div>
+
+
     </div>
 </template>
 
 <style scoped>
-h1 {
-    color: tomato;
-}
+
 </style>
