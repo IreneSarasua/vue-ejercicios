06f08b348c29c1fc212e96142dba05f3c75aa9b4
Irene Sarasua <irene.sarasua@ikasle.egibide.org>
2024-04-18 00:43:09 +0200

    directivas 9 cambio

--- 02_directivas/ej9/src/App.vue
+++ 02_directivas/ej9/src/App.vue
@@ -25,10 +25,17 @@ const borrarMensaje = (index) => {
     mensajes.value.splice(index, 1);
 }
 
-const estiloMensaje = 'w-100 border border-0';
+const estiloMensaje = 'w-100 border border-0 rounded focus-ring';
 const estiloEditando = 'w-100 border border-0 rounded focus-ring bg-success bg-opacity-25';
 const editar = (index) => {
-    mensajes.value[index].editando = !mensajes.value[index].editando;
+    //mensajes.value[index].editando = !mensajes.value[index].editando;
+    mensajes.value[index].editando = true;
+    document.getElementById(index).readonly = false;
+}
+
+const noEditar = (index) => {
+    mensajes.value[index].editando = false;
+    document.getElementById(index).readonly = true;
 }
 
 </script>
@@ -40,9 +47,13 @@ const editar = (index) => {
             <li class="list-group-item d-flex justify-content-between align-items-center"
                 v-for="(elem, index) of mensajes" :key="index">
                 <span class="w-100 h-100">
-
-                    <input :class="elem.editando? estiloEditando:estiloMensaje" 
-                           v-model="elem.texto" @focus="editar(index)" @focusout="editar(index)" v-bind:readonly>
+                    <!-- Como pone que se podra editar al hacer click, entiendo que deberia
+                     ser la única forma de poder editarlo. Así evitamos acceder mediante tabulador
+                     -->
+                    <input :class="elem.editando? estiloEditando:estiloMensaje" :id="index"
+                           v-model="elem.texto"
+                           @focus="noEditar(index)" @focusout="noEditar(index)"
+                           @click="editar(index)">
 
                 </span>
                 <span class="btn btn-primary rounded ms-2" @click="borrarMensaje(index)">
