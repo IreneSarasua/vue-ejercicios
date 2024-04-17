fb5b6961476ee6b502d8049976c2d2442bae4c74
Irene Sarasua <irene.sarasua@ikasle.egibide.org>
2024-04-17 10:03:14 +0200

    directivas 4

--- 02_directivas/ej4/src/App.vue
+++ 02_directivas/ej4/src/App.vue
@@ -1,16 +1,25 @@
 <script setup>
-const nombre = 'Nieves';
+    import { ref } from 'vue';
+    const  mostrar = ref(false);
+    const cambioEstado = () => {
+        mostrar.value = !mostrar.value
+    }
+
 </script>
 
 <template>
-    <div class="container pt-5">
-        <h1>¡Hola {{ nombre }}!</h1>
-        <p class="alert alert-success">Esto es una prueba.</p>
+    <div class="container w-50 pt-3">
+        <h2>Mensaje oculto</h2>
+
+        <div class="alert alert-success" v-show="mostrar">
+            ¡Hola, este es un mensaje!
+        </div>
+        <div class="btn btn-primary" @click="cambioEstado()">
+            Mostrar/Ocultar mensaje
+        </div>
     </div>
 </template>
 
 <style scoped>
-h1 {
-    color: tomato;
-}
+
 </style>
